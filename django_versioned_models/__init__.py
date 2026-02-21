d1ed5d407089287a479340dea03b0aef289a9952
avizaguri-macbook <avi210892@gmail.com>
2026-02-21 13:18:51 +0200

    Refactor imports in __init__.py for versioned models

--- django_versioned_models/__init__.py
+++ django_versioned_models/__init__.py
@@ -10,32 +10,11 @@ Quick start:
     INSTALLED_APPS = ['django_versioned_models', ...]
 
     # models.py
-    from django_versioned_models import VersionedModel
+    from django_versioned_models.mixins import VersionedModel
+    from django_versioned_models.models import Release
 
     class MyModel(VersionedModel):
         name = models.CharField(max_length=255)
-
-    # urls.py (optional, DRF required)
-    urlpatterns += [path('api/', include('django_versioned_models.urls'))]
-
-That's it. Run migrations and you're ready.
 """
 
-from django_versioned_models.mixins import DataStatus, VersionedModel
-from django_versioned_models.services import (
-    create_release,
-    get_versioned_models,
-    get_versioned_models_ordered,
-    lock_release,
-)
-
-__all__ = [
-    "VersionedModel",
-    "DataStatus",
-    "get_versioned_models",
-    "get_versioned_models_ordered",
-    "create_release",
-    "lock_release",
-]
-
-default_app_config = "django_versioned_models.apps.DjangoVersionedModelsConfig"
+default_app_config = 'django_versioned_models.apps.DjangoVersionedModelsConfig'
